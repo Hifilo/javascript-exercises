5348cec4e32eee971bdd6af515698b3a43287234
hifilo <92943726+Hifilo@users.noreply.github.com>
2023-01-08 14:09:28 -0500

    assignment 06 complete

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -3,7 +3,10 @@ const leapYears = function (...args) {
 	// console.log(args.every((args) => typeof args === `bigint`));
 	// console.log(typeof args[args]);
 
-	if (args % 4 === 0 && (args % 100 === 0) & (args % 400 === 0)) {
+	if (
+		(args % 4 === 0 && args % 100 !== 0) ||
+		(args % 100 === 0 && args % 400 === 0)
+	) {
 		console.log((leapYr = true));
 	} else {
 		console.log((leapYr = false));
@@ -17,7 +20,7 @@ const leapYears = function (...args) {
 // false[(1n, 2n, 3n)].every((x) => typeof x == 'bigint');
 // true;
 
-leapYears(34992);
+leapYears(700);
 /* x/4 = leap
 leap / 100 = no leap
 (leap/100)/400) = leap */
