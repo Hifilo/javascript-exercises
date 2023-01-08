7d29900bc0da13d44b43ec895e1c38a1d518f0d2
hifilo <92943726+Hifilo@users.noreply.github.com>
2023-01-08 14:00:52 -0500

    updated leapYears.js

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,25 @@
-const leapYears = function() {
+const leapYears = function (...args) {
+	let leapYr = false;
+	// console.log(args.every((args) => typeof args === `bigint`));
+	// console.log(typeof args[args]);
 
+	if (args % 4 === 0 && (args % 100 === 0) & (args % 400 === 0)) {
+		console.log((leapYr = true));
+	} else {
+		console.log((leapYr = false));
+		leapYr;
+	}
+	leapYr;
+	return leapYr;
 };
 
+// [1n, 2, 3n].every((x) => typeof x == 'bigint');
+// false[(1n, 2n, 3n)].every((x) => typeof x == 'bigint');
+// true;
+
+leapYears(34992);
+/* x/4 = leap
+leap / 100 = no leap
+(leap/100)/400) = leap */
 // Do not edit below this line
 module.exports = leapYears;
